1da05acde46dad234c13738a859cfca3db24b972
Pat Roque <169104185+pmvroque15@users.noreply.github.com>
2024-11-13 02:31:45 +0000

    navbar is working but not it's models

--- src/models.py
+++ src/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 # Character Model
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,10 +10,10 @@ class People(db.Model):
     homeworld = db.Column(db.String(250), nullable=False)
     birth_year = db.Column(db.String(250), nullable=False)
     hair_color = db.Column(db.String(250), nullable=False)
-    favorites = db.relationship('Favorites', back_populates='person')
+    favorites = db.relationship("Favorites", back_populates="person")
 
     def __repr__(self):
-        return f'<People {self.name}>'
+        return f"<People {self.name}>"
 
     def serialize(self):
         return {
@@ -23,16 +24,17 @@ class People(db.Model):
             "hair_color": self.hair_color,
         }
 
+
 # User Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(550), nullable=False, unique=True)
     is_active = db.Column(db.Boolean(), nullable=False)
-    favorites = db.relationship('Favorites', back_populates='user')
+    favorites = db.relationship("Favorites", back_populates="user")
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f"<User {self.email}>"
 
     def serialize(self):
         return {
@@ -41,16 +43,17 @@ class User(db.Model):
             "email": self.email,
         }
 
+
 # Planet Model
 class Planets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False, unique=True)
     terrain = db.Column(db.String(250), nullable=True)
     population = db.Column(db.Integer, nullable=True)
-    favorites = db.relationship('Favorites', back_populates='planet')
+    favorites = db.relationship("Favorites", back_populates="planet")
 
     def __repr__(self):
-        return f'<Planets {self.name}>'
+        return f"<Planets {self.name}>"
 
     def serialize(self):
         return {
@@ -60,18 +63,19 @@ class Planets(db.Model):
             "population": self.population,
         }
 
+
 # Favorites Model
 class Favorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
-    person = db.relationship('People', back_populates='favorites')
-    user = db.relationship('User', back_populates='favorites')
-    planet = db.relationship('Planets', back_populates='favorites')
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    planets_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
+    person = db.relationship("People", back_populates="favorites")
+    user = db.relationship("User", back_populates="favorites")
+    planet = db.relationship("Planets", back_populates="favorites")
 
     def __repr__(self):
-        return f'<Favorites id={self.id}>'
+        return f"<Favorites id={self.id}>"
 
     def serialize(self):
         return {
